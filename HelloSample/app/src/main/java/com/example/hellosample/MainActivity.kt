4817c729781c61cfb0ef047feb04df7adb112bb6
imakyo97 <kyoheishogun.1997@gmail.com>
2024-01-22 16:58:22 +0900

    イベントとリスナを実装

--- HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -1,43 +1,41 @@
 package com.example.hellosample
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.hellosample.ui.theme.HelloSampleTheme
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HelloSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListener()
+        btClick.setOnClickListener(listener)
+
+        val btClear = findViewById<Button>(R.id.btClear)
+        btClear.setOnClickListener(listener)
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View?) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            when (view?.id) {
+                R.id.btClick -> {
+                    val inputStr = input.text.toString()
+                    output.text = inputStr + "さん、こんにちは！"
+                }
+                R.id.btClear -> {
+                    input.setText("")
+                    output.text = ""
                 }
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelloSampleTheme {
-        Greeting("Android")
     }
 }
