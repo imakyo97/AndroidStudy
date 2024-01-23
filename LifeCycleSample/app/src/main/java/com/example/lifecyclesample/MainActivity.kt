14c1b46ae3327b90082d00b6b79372b59409f24e
imakyo97 <kyoheishogun.1997@gmail.com>
2024-01-24 00:53:01 +0900

    ライフサイクルの処理を実装

--- LifeCycleSample/app/src/main/java/com/example/lifecyclesample/MainActivity.kt
+++ LifeCycleSample/app/src/main/java/com/example/lifecyclesample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lifecyclesample
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +10,42 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Main onCreate() called.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btNext = findViewById<Button>(R.id.btNext)
         btNext.setOnClickListener(NextListener())
+    }
+
+    public override fun onStart() {
+        Log.i("LifeCycleSample", "Main onStart() called.")
+        super.onStart()
+    }
+
+    public override fun onRestart() {
+        Log.i("LifeCycleSample", "Main onRestart() called.")
+        super.onRestart()
+    }
+
+    public override fun onResume() {
+        Log.i("LifeCycleSample", "Main onResume() called.")
+        super.onResume()
+    }
+
+    public override fun onPause() {
+        Log.i("LifeCycleSample", "Main onPause() called.")
+        super.onPause()
+    }
+
+    public override fun onStop() {
+        Log.i("LifeCycleSample", "Main onStop() called.")
+        super.onStop()
+    }
+
+    public override fun onDestroy() {
+        Log.i("LifeCycleSample", "Main onDestory() called.")
+        super.onDestroy()
     }
 
     private inner class NextListener: View.OnClickListener {
--- LifeCycleSample/app/src/main/java/com/example/lifecyclesample/SecondActivity.kt
+++ LifeCycleSample/app/src/main/java/com/example/lifecyclesample/SecondActivity.kt
@@ -3,16 +3,48 @@ package com.example.lifecyclesample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Sub onCreate() called.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         val btBack = findViewById<Button>(R.id.btBack)
         btBack.setOnClickListener(BackListener())
+    }
+
+    public override fun onStart() {
+        Log.i("LifeCycleSample", "Sub onStart() called.")
+        super.onStart()
+    }
+
+    public override fun onRestart() {
+        Log.i("LifeCycleSample", "Sub onRestart() called.")
+        super.onRestart()
+    }
+
+    public override fun onResume() {
+        Log.i("LifeCycleSample", "Sub onResume() called.")
+        super.onResume()
+    }
+
+    public override fun onPause() {
+        Log.i("LifeCycleSample", "Sub onPause() called.")
+        super.onPause()
+    }
+
+    public override fun onStop() {
+        Log.i("LifeCycleSample", "Sub onStop() called.")
+        super.onStop()
+    }
+
+    public override fun onDestroy() {
+        Log.i("LifeCycleSample", "Sub onDestory() called.")
+        super.onDestroy()
     }
 
     inner class BackListener: View.OnClickListener {
