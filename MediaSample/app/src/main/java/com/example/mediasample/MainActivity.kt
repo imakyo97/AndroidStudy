8413d3927eba7a6b79fc02f68e65958ec815c8e4
imakyo97 <kyoheishogun.1997@gmail.com>
2024-01-25 22:19:56 +0900

    音声の再生・停止機能を実装

--- MediaSample/app/src/main/java/com/example/mediasample/MainActivity.kt
+++ MediaSample/app/src/main/java/com/example/mediasample/MainActivity.kt
@@ -1,12 +1,101 @@
 package com.example.mediasample
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 
 class MainActivity : AppCompatActivity() {
+    private var _player: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _player = MediaPlayer()
+        val mediaFileUriStr = "android.resource://${packageName}/${R.raw.bird}"
+        val mediaFileUri = Uri.parse(mediaFileUriStr)
+        _player?.let {
+            it.setDataSource(this@MainActivity, mediaFileUri)
+            it.setOnPreparedListener(PlayerPreparedListener())
+            it.setOnCompletionListener(PlayerCompletionListener())
+            it.prepareAsync()
+        }
+
+        val loopSwitch = findViewById<SwitchMaterial>(R.id.swLoop)
+        loopSwitch.setOnCheckedChangeListener(LoopSwitchChangeListener())
+    }
+
+    override fun onStop() {
+        _player?.let {
+            if (it.isPlaying) {
+                it.stop()
+            }
+            it.release()
+        }
+        super.onStop()
+    }
+
+    private inner class PlayerCompletionListener : MediaPlayer.OnCompletionListener {
+        override fun onCompletion(mp: MediaPlayer?) {
+            _player?.let {
+                if (!it.isLooping) {
+                    val btPlay = findViewById<Button>(R.id.btPlay)
+                    btPlay.setText(R.string.bt_play_play)
+                }
+            }
+        }
+    }
+
+    private inner class PlayerPreparedListener : MediaPlayer.OnPreparedListener {
+        override fun onPrepared(mp: MediaPlayer?) {
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            btPlay.isEnabled = true
+            val btBack = findViewById<Button>(R.id.btBack)
+            btBack.isEnabled = true
+            val btForward = findViewById<Button>(R.id.btForward)
+            btForward.isEnabled = true
+        }
+    }
+
+    private inner class LoopSwitchChangeListener: CompoundButton.OnCheckedChangeListener {
+        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+            _player?.isLooping = isChecked
+        }
+    }
+
+    fun onPlayButtonClick(view: View) {
+        _player?.let {
+            Log.d("DEBAG", "ボタンがタップされました")
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            if (it.isPlaying) {
+                it.pause()
+                btPlay.setText(R.string.bt_play_play)
+            } else {
+                it.start()
+                btPlay.setText(R.string.bt_play_pause)
+            }
+        }
+    }
+
+    fun onBackButtonClick(view: View) {
+        _player?.seekTo(0)
+    }
+
+    fun onForwardButtonClick(view: View) {
+        _player?.let {
+            val duration = it.duration
+            it.seekTo(duration)
+            if(!it.isPlaying) {
+                val btPlay = findViewById<Button>(R.id.btPlay)
+                btPlay.setText(R.string.bt_play_pause)
+                it.start()
+            }
+        }
     }
 }
