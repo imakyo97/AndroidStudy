a74300d4b412a8fa42c7f4715dfdfafcc14f25e2
imakyo97 <kyoheishogun.1997@gmail.com>
2024-01-25 10:42:38 +0900

    sqliteのコードを追加

--- DatabaseSample/app/src/main/java/com/example/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/com/example/databasesample/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.example.databasesample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var _cocktaild = -1
+    private var _cocktailName = ""
+
+    private val _helper = DatabaseHelper(this@MainActivity)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvCoctail = findViewById<ListView>(R.id.lvCocktail)
+        lvCoctail.onItemClickListener = ListItemClickListener()
+    }
+
+    override fun onDestroy() {
+        _helper.close()
+        super.onDestroy()
+    }
+
+    fun onSaveButtonClick(view: View) {
+        val etNote = findViewById<EditText>(R.id.etNote)
+        val note = etNote.text.toString()
+
+        val db = _helper.writableDatabase
+        val sqlDelete = "DELETE FROM cocktailmemos WHERE _id = ?"
+        var stmt = db.compileStatement(sqlDelete)
+        stmt.bindLong(1, _cocktaild.toLong())
+        stmt.executeUpdateDelete()
+
+        val sqlInsert = "INSERT INTO cocktailmemos (_id, name, note) VALUES (?, ?, ?)"
+        stmt = db.compileStatement(sqlInsert)
+        stmt.bindLong(1, _cocktaild.toLong())
+        stmt.bindString(2, _cocktailName)
+        stmt.bindString(3, note)
+        stmt.executeInsert()
+
+        etNote.setText("")
+        val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
+        tvCocktailName.text = getString(R.string.tv_name)
+        val btSave = findViewById<Button>(R.id.btSave)
+        btSave.isEnabled = false
+    }
+
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            _cocktaild = position
+            _cocktailName = parent?.getItemAtPosition(position) as String
+            val tvCocktailName = findViewById<TextView>(R.id.tvCocktailName)
+            tvCocktailName.text = _cocktailName
+            val btSave = findViewById<Button>(R.id.btSave)
+            btSave.isEnabled = true
+
+            val db = _helper.writableDatabase
+            val sql = "SELECT * FROM cocktailmemos WHERE _id = ${_cocktaild}"
+            val cursor = db.rawQuery(sql, null)
+
+            var note = ""
+            while (cursor.moveToNext()) {
+                val idxNote = cursor.getColumnIndex("note")
+                note = cursor.getString(idxNote)
+            }
+
+            val etNote = findViewById<EditText>(R.id.etNote)
+            etNote.setText(note)
+        }
     }
 }
