af168c876635e991803aec938fb6f40550c2e3e2
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2024-01-29 12:24:19 +0900

    editTextのリスナーを実装

--- WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
+++ WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.weatherappsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import com.example.weatherappsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.etCity.setOnEditorActionListener(OnCityEditorActionListener())
+    }
+
+    private inner class OnCityEditorActionListener : TextView.OnEditorActionListener {
+        override fun onEditorAction(p0: TextView?, p1: Int, p2: KeyEvent?): Boolean {
+            if(p1 == EditorInfo.IME_ACTION_DONE) {
+                // TODO: 通信処理を実行
+                return true
+            }
+            return false
+        }
+
     }
 }
