7e9f003e840f831fcb099be3281e679ce2cb8ab1
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2024-01-30 00:21:35 +0900

    通信処理と結果表示を実装

--- WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
+++ WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.example.weatherappsample.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
+
+    private val apiClient = ApiClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +25,45 @@ class MainActivity : AppCompatActivity() {
     private inner class OnCityEditorActionListener : TextView.OnEditorActionListener {
         override fun onEditorAction(p0: TextView?, p1: Int, p2: KeyEvent?): Boolean {
             if (p1 == EditorInfo.IME_ACTION_DONE) {
-                // TODO: 通信処理を実行
-                return true
+                lifecycleScope.launch {
+                    val weatherData = apiClient.fetchWeather(p0?.text.toString())
+                    showWeatherData(weatherData)
+                }
+                return false
             }
             return false
         }
+    }
 
+    private fun showWeatherData(data: WeatherData) {
+        activityMainBinding.tvCity.text = data.name
+        activityMainBinding.imageView.setImageResource(getIconResource(data.weather.first().icon))
+        activityMainBinding.tvWeather.text = data.weather.first().description
+        activityMainBinding.tvTempMax.text = "${data.main.tempMax} ℃"
+        activityMainBinding.tvTempMin.text = "${data.main.tempMin} ℃"
+    }
+
+    private fun getIconResource(icon: String): Int {
+        return when (icon) {
+            "01d" -> R.drawable.d01
+            "01n" -> R.drawable.n01
+            "02d" -> R.drawable.d02
+            "02n" -> R.drawable.n02
+            "03d" -> R.drawable.d03
+            "03n" -> R.drawable.n03
+            "04d" -> R.drawable.d04
+            "04n" -> R.drawable.n04
+            "09d" -> R.drawable.d09
+            "09n" -> R.drawable.n09
+            "10d" -> R.drawable.d10
+            "10n" -> R.drawable.n10
+            "11d" -> R.drawable.d11
+            "11n" -> R.drawable.n11
+            "13d" -> R.drawable.d13
+            "13n" -> R.drawable.n13
+            "50d" -> R.drawable.d50
+            "50n" -> R.drawable.n50
+            else -> R.drawable.d01
+        }
     }
 }
