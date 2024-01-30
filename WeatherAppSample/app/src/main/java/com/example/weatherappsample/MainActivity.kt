f6d8fa415f8798d77b7b5c98b6a46519d98e5a2c
imakyo97 <kyoheishogun.1997@gmail.com>
2024-01-30 16:27:15 +0900

    エラー処理を実装

--- WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
+++ WeatherAppSample/app/src/main/java/com/example/weatherappsample/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.weatherappsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.weatherappsample.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
@@ -26,44 +27,49 @@ class MainActivity : AppCompatActivity() {
         override fun onEditorAction(p0: TextView?, p1: Int, p2: KeyEvent?): Boolean {
             if (p1 == EditorInfo.IME_ACTION_DONE) {
                 lifecycleScope.launch {
-                    val weatherData = apiClient.fetchWeather(p0?.text.toString())
-                    showWeatherData(weatherData)
+                    kotlin.runCatching { apiClient.fetchWeather(p0?.text.toString()) }
+                        .onSuccess { weatherData ->
+                            showWeatherData(weatherData)
+                        }
+                        .onFailure { error ->
+                            Log.d("DEBUG", "error: $error")
+                        }
                 }
                 return false
             }
             return false
         }
-    }
 
-    private fun showWeatherData(data: WeatherData) {
-        activityMainBinding.tvCity.text = data.name
-        activityMainBinding.imageView.setImageResource(getIconResource(data.weather.first().icon))
-        activityMainBinding.tvWeather.text = data.weather.first().description
-        activityMainBinding.tvTempMax.text = "${data.main.tempMax} ℃"
-        activityMainBinding.tvTempMin.text = "${data.main.tempMin} ℃"
-    }
+        private fun showWeatherData(data: WeatherData) {
+            activityMainBinding.tvCity.text = data.name
+            activityMainBinding.imageView.setImageResource(getIconResource(data.weather.first().icon))
+            activityMainBinding.tvWeather.text = data.weather.first().description
+            activityMainBinding.tvTempMax.text = "${data.main.tempMax} ℃"
+            activityMainBinding.tvTempMin.text = "${data.main.tempMin} ℃"
+        }
 
-    private fun getIconResource(icon: String): Int {
-        return when (icon) {
-            "01d" -> R.drawable.d01
-            "01n" -> R.drawable.n01
-            "02d" -> R.drawable.d02
-            "02n" -> R.drawable.n02
-            "03d" -> R.drawable.d03
-            "03n" -> R.drawable.n03
-            "04d" -> R.drawable.d04
-            "04n" -> R.drawable.n04
-            "09d" -> R.drawable.d09
-            "09n" -> R.drawable.n09
-            "10d" -> R.drawable.d10
-            "10n" -> R.drawable.n10
-            "11d" -> R.drawable.d11
-            "11n" -> R.drawable.n11
-            "13d" -> R.drawable.d13
-            "13n" -> R.drawable.n13
-            "50d" -> R.drawable.d50
-            "50n" -> R.drawable.n50
-            else -> R.drawable.d01
+        private fun getIconResource(icon: String): Int {
+            return when (icon) {
+                "01d" -> R.drawable.d01
+                "01n" -> R.drawable.n01
+                "02d" -> R.drawable.d02
+                "02n" -> R.drawable.n02
+                "03d" -> R.drawable.d03
+                "03n" -> R.drawable.n03
+                "04d" -> R.drawable.d04
+                "04n" -> R.drawable.n04
+                "09d" -> R.drawable.d09
+                "09n" -> R.drawable.n09
+                "10d" -> R.drawable.d10
+                "10n" -> R.drawable.n10
+                "11d" -> R.drawable.d11
+                "11n" -> R.drawable.n11
+                "13d" -> R.drawable.d13
+                "13n" -> R.drawable.n13
+                "50d" -> R.drawable.d50
+                "50n" -> R.drawable.n50
+                else -> R.drawable.d01
+            }
         }
     }
 }
